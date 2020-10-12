19ca56ba358cabb0347369e434a510d754572c04
stephenh0191 <stephenhughes0191@gmail.com>
2020-10-12 20:25:36 +0100

    add new and create with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :message
-  root 'messages#index'
+  
+  resources :messages
+  root 'message#index'
 end
