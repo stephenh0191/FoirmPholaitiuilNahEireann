aaa46bfbd2e71617cc68626b208809caa44053c5
stephenh0191 <stephenhughes0191@gmail.com>
2020-10-12 20:57:43 +0100

    Add Index and show pages actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,17 +2,21 @@ class MessagesController < ApplicationController
 	
 
 	def index
+		@messages = Message.all.order("created_at DESC")
 	end 
 
 	def new
 		@message = Message.new
 	end
 
+	def show
+		@message = Message.find(params[:id])
+	end
 
 	def create
 		@message = Message.new(message_params)
 		if @message.save
-		redirect_to rooth_path
+		redirect_to root_path
 		else
 		render 'new'
 		end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   
   resources :messages
-  root 'message#index'
+  root 'messages#index'
 end
