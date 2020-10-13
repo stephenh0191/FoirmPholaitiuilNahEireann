aa4a674851d7c0d847c62d289be2b3c7b6bb0d54
stephenh0191 <stephenhughes0191@gmail.com>
2020-10-13 21:59:29 +0100

    add users to id

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_195458) do
+ActiveRecord::Schema.define(version: 2020_10_13_204024) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
